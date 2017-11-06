0674e846bb26d3d9f8cda7d61635854a65dbca5b
Pablo Alcocer <palco003@fiu.edu>
2017-11-06 00:42:19 -0500

    Add defaults to AdminStats

--- dev/classic/src/view/stats/AdminStats.js
+++ dev/classic/src/view/stats/AdminStats.js
@@ -15,6 +15,16 @@ Ext.define('MobileJudge.view.stats.AdminStats', {
     },
     layout: 'responsivecolumn',
 
+    defaultType: 'basechart',
+    defaults: {
+        iconCls: 'x-fa fa-pie-chart',
+        userCls: 'big-33 small-100',
+        height: 300,
+        defaults: {
+            animation : !Ext.isIE9m && Ext.os.is.Desktop
+        }
+    },
+
     items: [
         {
             xtype:'gradeByProject',
