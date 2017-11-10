abfbeddc2272f9e2a26b084b3aa51bd7170ddf86
Pablo Alcocer <palco003@fiu.edu>
2017-11-10 16:59:21 -0500

    change model to store -- mistake

--- dev/app/view/stats/GradeByProject.js
+++ dev/app/view/stats/GradeByProject.js
@@ -72,7 +72,7 @@ Ext.define('MobileJudge.view.stats.GradeByProject', {
                         //     panel.setHtml('Project: ' + item.record.get('project'));
                         // },
                         itemclick: function(chart, item) {
-                            var store = Ext.getStore('MobileJudge.store.grade.Student');
+                            var store = Ext.getStore('MobileJudge.store.grade.Students');
                             var msg = "<br>-------------------------------";
                             console.log('store' + store);
 
