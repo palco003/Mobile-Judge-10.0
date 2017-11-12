296699f8344d26d3d7df23e44a9155c15c7b2bac
Pablo Alcocer <palco003@fiu.edu>
2017-11-12 17:02:33 -0500

    revert to createbyalias

--- dev/app/store/stats/GroupedProjects.js
+++ dev/app/store/stats/GroupedProjects.js
@@ -1017,17 +1017,11 @@ Ext.define('MobileJudge.store.stats.GradeAverage', {
     model: 'MobileJudge.model.stats.GradeAverage',
     data: (function () {
         var data = [];
-        // var expense = Ext.createByAlias('store.projectGrades');
-        var store = Ext.getStore('projectGrades').data.items;
+        var store = Ext.createByAlias('store.projectGrades');
+        // var store = Ext.getStore('projectGrades').data.items;
         console.log('store before group ' + store);
-        // expense.group('project');
         store.group('project');
         var groups = store.getGroups();
-        // var groups = expense.getGroups();
-        // for(var i = 0; i < groups.getCount(); i++){
-        //
-        // }
-        // console.log(groups);
         groups.each(function (group) {
             console.log(group.config.groupKey);
             console.log(group);
