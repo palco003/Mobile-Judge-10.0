a33cbc426a98cb1737570a0c75f072c8ae9f9ff9
Pablo Alcocer <palco003@fiu.edu>
2017-11-16 02:28:17 -0500

    remove console logs

--- dev/app/store/stats/GroupedProjects.js
+++ dev/app/store/stats/GroupedProjects.js
@@ -974,36 +974,12 @@ Ext.define('MobileJudge.store.stats.GradeAverage', {
     model: 'MobileJudge.model.stats.GradeAverage',
     data: (function () {
         var data = [];
-        var store = Ext.createByAlias('store.students');
-        var jsonStore = Ext.createByAlias('store.projectGrades');
-
-        console.log('store.students');
-        console.log(store);
-
-        console.log('jsonStore - project grades');
-        console.log(jsonStore);
+        var store = Ext.createByAlias('store.projectGrades');
 
         store.group('project');
         var groups = store.getGroups();
 
-        jsonStore.group('project');
-        var jsonStoreGroups = jsonStore.getGroups();
-
-        console.log('groups');
-        console.log(groups);
-
-        console.log('jsonStoreGroups');
-        console.log(jsonStoreGroups);
-
-        console.log('equality');
-        console.log('==');
-        console.log(groups == jsonStoreGroups);
-        console.log('===');
-        console.log(groups === jsonStoreGroups);
-
         groups.each(function (group) {
-            console.log(group.config.groupKey);
-            console.log(group);
             data.push({
                 project: group.config.groupKey, // each group is a project
                 avgGrade: group.average('grade'),
