64a3805504949f647ce50726f0a81857b94538cc
Pablo Alcocer <palco003@fiu.edu>
2017-12-01 15:53:52 -0500

    fix admin stats

--- dev/classic/src/view/stats/AdminStats.js
+++ dev/classic/src/view/stats/AdminStats.js
@@ -27,10 +27,6 @@ Ext.define('MobileJudge.view.stats.AdminStats', {
 
     items: [
         {
-            xtype:'testingCharts',
-            userCls: 'big-100 small-100'
-        },
-        {
             xtype:'gradeByProject',
             userCls: 'big-100 small-100'
         },
