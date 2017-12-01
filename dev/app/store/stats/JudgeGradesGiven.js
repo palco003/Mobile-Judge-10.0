a9419ee37f6c99f5f872742d30083a2b8f1bd805
Pablo Alcocer <palco003@fiu.edu>
2017-12-01 13:30:20 -0500

    add console logs

--- dev/app/store/stats/JudgeGradesGiven.js
+++ dev/app/store/stats/JudgeGradesGiven.js
@@ -9,31 +9,31 @@ Ext.create('Ext.data.Store', {
     fields:['judgeId', 'studentId', 'question', 'grade'],
     data:[]
 });
-
-Ext.define('MobileJudge.model.stats.JudgeQuestionAverage', {
-    extend: 'Ext.data.Model',
-    fields: ['question', 'average']
-});
-
-
-Ext.define('MobileJudge.store.stats.JudgeQuestionAverage', {
-    extend: 'Ext.data.Store',
-    alias: 'store.judgeQuestionAverage',
-    model: 'MobileJudge.model.stats.JudgeQuestionAverage',
-    data: (function () {
-        var data = [];
-        var store = Ext.createByAlias('store.judgeGradesGiven');
-        store.group('question');
-        var groups = store.getGroups();
-        groups.each(function (group) {
-            data.push({
-                question: group.config.groupKey,
-                average: group.average('grade')
-            });
-        });
-        return data;
-    })()
-});
+//
+// Ext.define('MobileJudge.model.stats.JudgeQuestionAverage', {
+//     extend: 'Ext.data.Model',
+//     fields: ['question', 'average']
+// });
+//
+//
+// Ext.define('MobileJudge.store.stats.JudgeQuestionAverage', {
+//     extend: 'Ext.data.Store',
+//     alias: 'store.judgeQuestionAverage',
+//     model: 'MobileJudge.model.stats.JudgeQuestionAverage',
+//     data: (function () {
+//         var data = [];
+//         var store = Ext.createByAlias('store.judgeGradesGiven');
+//         store.group('question');
+//         var groups = store.getGroups();
+//         groups.each(function (group) {
+//             data.push({
+//                 question: group.config.groupKey,
+//                 average: group.average('grade')
+//             });
+//         });
+//         return data;
+//     })()
+// });
 
 Ext.define('MobileJudge.view.grade.JudgeGradesChart', {
     extend: 'MobileJudge.view.stats.Base',
